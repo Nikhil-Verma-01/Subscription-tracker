7c3008022531546234ee01fe120a73f53854645a
Nikhil-Verma-01 <nik.h0ill907@gmail.com>
2025-06-05 22:41:53 +0530

    Iniiating into git

---
+++ config/env.js
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+
+// eslint-disable-next-line no-undef
+config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
+
+export const {
+    PORT,
+    NODE_ENV,
+    DB_URI,
+    JWT_SECRET, JWT_EXPIRE_IN
+    ARCJET_ENV, ARCJET_KEY
+// eslint-disable-next-line no-undef
+}  = process.env;
---
+++ routes/auth.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { signUp } from "../controllers/auth.controller";
+
+const authRouter = Router();
+
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', (req, res) => {
+    res.send({title: 'Sign In'});
+});
+authRouter.post('/sign-out', (req, res) => {
+    res.send({title: 'Sign out'});
+});
+
+export default authRouter;
